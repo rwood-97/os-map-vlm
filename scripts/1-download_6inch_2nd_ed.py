bbec2979cd008c5a9d151844acd71002a7401f14
Rosie Wood <rwood@turing.ac.uk>
2026-05-22 12:59:11 +0100

    add download 6 inch 2nd ed script and metadata

---
+++ scripts/1-download_6inch_2nd_ed.py
@@ -0,0 +1,36 @@
+import os
+
+import geopandas as gpd
+import numpy as np
+from mapreader.download.sheet_downloader import SheetDownloader
+
+# get maptiler key
+maptiler_key = os.getenv("MAPTILER_KEY")
+
+# load NLS metadata
+metadata = gpd.read_file("../data/metadata_nls_six_inch_2nd_ed.geojson")
+bounds = metadata.bounds
+
+# Split y (latitude) into 10 bands, sample 200 sheets per band (~2,000 total)
+ys = np.linspace(metadata.total_bounds[1], metadata.total_bounds[3], 11)
+
+samples = []
+for y_min, y_max in zip(ys[:-1], ys[1:], strict=True):
+    sample = metadata[(bounds["miny"] <= y_max) & (y_min <= bounds["maxy"])]
+    sample = sample.sample(n=250, random_state=42) if len(sample) > 250 else sample
+    samples.append(sample)
+
+print(f"Total samples: {sum(len(sample) for sample in samples)}")
+
+# define sheet downloader
+downloader = SheetDownloader(
+    "../data/metadata_nls_six_inch_2nd_ed.geojson",
+    "https://api.maptiler.com/tiles/uk-osgb10k1888/{z}/{x}/{y}.jpg?key=" + maptiler_key
+)
+
+# download sheets for each sample
+for sample in samples:
+    # for testing, just download 2 sheets per sample
+    downloader.metadata = sample.sample(n=2, random_state=42)
+    downloader.get_grid_bb()
+    downloader.download_all_map_sheets("../data/maps_6inch_2nd_ed", download_in_parrallel=True)
