1adddf46f60eb6b933d8a8e97f78d1131baaa931
Rosie Wood <rwood@turing.ac.uk>
2026-05-27 14:30:43 +0000

    sample town plans and 25 inch

--- scripts/4-download_town_plans_500.py
+++ scripts/4-download_town_plans_500.py
@@ -1,10 +1,11 @@
 import geopandas as gpd
 from mapreader.download.sheet_downloader import SheetDownloader
 
-# load NLS metadata
+# load NLS metadata, sample 4 sheets per town
 metadata = gpd.read_file("./data/metadata_ostowns_500_with_xyz.json")
+metadata = metadata.groupby("TWN", group_keys=False).apply(lambda x: x.sample(min(4, len(x)), random_state=42)).reset_index(drop=True)
 
-# define sheet downloader (tile server is set per county layer below)
+# define sheet downloader (tile server is set per layer below)
 downloader = SheetDownloader(
     "./data/metadata_ostowns_500_with_xyz.json",
     "https://mapseries-tilesets.s3.amazonaws.com/os/town-england/Wales/{z}/{x}/{y}.png"
