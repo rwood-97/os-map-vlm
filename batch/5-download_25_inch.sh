2c8ac9342a23dbd1c604eba4e0ace4ccbe18874a
Rosie Wood <rwood@turing.ac.uk>
2026-05-27 14:36:47 +0000

    add batch script for 25 inch

---
+++ batch/5-download_25_inch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=download_25_inch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --error=logs/%x-%j.err
+
+module load cuda/12.6
+module load gcc-native/12.3
+
+source .env
+
+uv run --no-sync python ./scripts/5-download_25_inch.py
